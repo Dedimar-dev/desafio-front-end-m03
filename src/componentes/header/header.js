e233536a01a97ae763f0ba879f9f211dfdf42066
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-21 11:04:45 -0300

    Nova mudança

--- src/componentes/header/header.js
+++ src/componentes/header/header.js
@@ -1,13 +1,13 @@
 import './header.css';
 import logo from './assets/Group5.svg';
 
-function Header(){
-    return(
+function Header() {
+    return (
         <header className="container-header">
             <div className="container-header-div">
-                <img  src={logo} alt="logo" />
-                <h1>Dindin</h1>    
-            </div> 
+                <img src={logo} alt="logo" />
+                <h1>Dindin</h1>
+            </div>
         </header>
     )
 }
